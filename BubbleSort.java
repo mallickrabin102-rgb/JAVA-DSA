cf984e61135e3643340540206ca4ecf7c6d676f0
mallickrabin102-rgb <mallickrabin102@gmail.com>
2026-03-11 00:25:57 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    static void bubblesort(int arr[]) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length- i-1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 2, 8, 1, 9 };
+bubblesort(arr);
+for(int i=0;i<arr.length;i++){
+    System.out.println(arr[i]);
+}
+    }
+}
