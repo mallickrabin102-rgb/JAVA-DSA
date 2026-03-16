a3fcfc40fe90ae5393ac3df8a3f0a8eda86295da
mallickrabin102-rgb <mallickrabin102@gmail.com>
2026-03-16 22:22:36 +0530

    Update MaxElement.java

--- MaxElement.java
+++ MaxElement.java
@@ -1,8 +1,8 @@
 public class MaxElement {
     public static void main(String[] args){
         int[] arr = {5, 8, 7, 9, 10};
-        int Max = Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
+        int Max = arr[0];
+        for(int i=1;i<arr.length;i++){
 if (arr[i]>Max) {
     Max = arr[i];
 }
